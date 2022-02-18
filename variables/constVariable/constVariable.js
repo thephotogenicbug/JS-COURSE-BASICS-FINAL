1881d5e2f11992615b6911748191c0b9d8b1f1a2
thephotogenicbug <navinkumarachari@gmail.com>
2022-02-18 11:44:39 +0530

    18-02-2022 : const variable example

---
+++ variables/constVariable/constVariable.js
@@ -0,0 +1,27 @@
+/*
+    name variable is declared using let keyword
+    if you create any variable using let keyword u can change there value 
+    if you create any variable using const keyword u can't change there value 
+*/
+// let keyword example
+let fname = "Naveen";
+fname = "Kumar"
+let msg = "Hello my name is " + fname + " i'm learning javascript"; // message variable
+
+// Logging
+console.log(msg); // statement
+
+console.log("----------------------------------------------------------------")
+
+/* const keyword example 
+   if you declare any variable in const keyword it will be permament 
+   u can't change / modify there value
+*/ 
+const name = "Naveen Kumar";
+// name = "Kumar"
+let message = "Hello my name is " + name + " i'm learning javascript"; // message variable
+
+// Logging
+console.log(message); // statement
+
+
---
+++ variables/constVariable/constVariable1.js
@@ -0,0 +1,6 @@
+const name = "Naveen";
+const dob = "05/01/1997";
+
+// concatinate name and dob 
+let msg = "Hello my name is " + name +  " and i was born on " + dob + " I am Leaning JavaScript"
+console.log(msg)
