f8de221138c851fcc8ea788331c36786e9baa9f7
thephotogenicbug <navinkumarachari@gmail.com>
2022-02-18 12:45:22 +0530

    18-02-2022 : JS Arrays Example

---
+++ arrays/arrays.js
@@ -0,0 +1,15 @@
+// use square bracket [] to create an array by default it will be empty array
+let favColors = ["red", "white", "black"]
+
+
+// add value dynamically 
+favColors[4] = "Orange"
+
+// Print all the values in console
+console.log(favColors)
+console.log("------------------------------------")
+// Print single value using index
+console.log(favColors[0])
+console.log("-------------------------------------")
+// print length of the values
+console.log(favColors.length)
