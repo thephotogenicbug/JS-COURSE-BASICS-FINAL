7bedca5c18a4c81fc7c75a4bdd49a19cd8d4dab7
thephotogenicbug <navinkumarachari@gmail.com>
2022-02-18 12:09:00 +0530

    18-02-2022 : JS Data-Types Examples

---
+++ dataTypes/data-type-list.js
@@ -0,0 +1,15 @@
+/*
+  List of data-types
+  
+  Primitive-Types
+  * String
+  * Integer / Number
+  * Boolean
+  * undefined
+  * null
+
+  Reference-Types
+  * Object
+  * Array
+  * Function
+*/
---
+++ dataTypes/data-types.js
@@ -0,0 +1,33 @@
+// = is called assigment operator
+let firstName = "Naveen"; // String literal
+let age = 24; // Integer literal
+let isYoung = true; // Boolean
+let lastName = undefined; // undefined
+let trophy = null; // null by default trophy variable will be null
+
+/* 
+  null will wrap inside an object, object will bundle ur set of informations
+  it can be string, number, boolean, etc etc  we will learn more about object 
+  in reference types
+*/
+
+// trophy variable example
+// trophy = "Some trophy"
+
+/*
+  Create typeof.html and connect data-type.js and explain how browser understands this data types 
+  by running typeof keyword
+*/
+
+/*
+   Programming concept
+
+   Statically Typed languages c, c# c++ java
+
+   String name = "Naveen" // String
+
+   Dynamic Typed languages JavaScript, python, php 
+
+   name = "Naveen" // String
+
+*/
